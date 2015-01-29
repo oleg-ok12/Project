ad001eb6129f4e73e2e12de6393a95eca60b8b11
oleg-ok12 <oleg.ok12@gmail.com>
2015-01-29 01:15:40 +0100

    troche NCC

---
+++ NetworkCallController/NetworkCallController/Serialization.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace NetworkCallController
+{
+    static class Serialization
+    {
+        public static string SerializeObject(Object obj)
+        {
+            String xml_string = null;
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+
+            StringBuilder sb = new StringBuilder();
+            StringWriter string_writer = new StringWriter(sb);
+            serializer.Serialize(string_writer, obj);
+            xml_string = sb.ToString();
+            Console.WriteLine("Serialization done");
+            return xml_string;
+        }
+
+        public static Object DeserializeObject(String xml_string, Type type)
+        {
+            XmlSerializer xs = new XmlSerializer(type);
+            StringReader reader = new StringReader(xml_string);
+            XmlTextReader xmlTextReader = new XmlTextReader(reader);
+            return xs.Deserialize(xmlTextReader);
+        }
+
+        public static void SerializeToFile(Object obj, string fileName)
+        {
+            if (obj == null) { return; }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(obj.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, obj);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(fileName);
+                    stream.Close();
+
+                    Console.WriteLine("Serialization done.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log exception here
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static Object DeSerializeFromFile(string fileName, Type type)
+        {
+            if (string.IsNullOrEmpty(fileName)) { return default(Object); }
+
+            Object objectOut = default(Object);
+
+            try
+            {
+                string attributeXml = string.Empty;
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(fileName);
+                string xmlString = xmlDocument.OuterXml;
+
+                using (StringReader read = new StringReader(xmlString))
+                {
+                    //Type outType = typeof(Object);
+
+                    XmlSerializer serializer = new XmlSerializer(type);
+                    using (XmlReader reader = new XmlTextReader(read))
+                    {
+                        objectOut = serializer.Deserialize(reader);
+                        reader.Close();
+                    }
+
+                    read.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log exception here
+                Console.WriteLine(ex);
+            }
+
+            return objectOut;
+        }
+
+
+
+    }
+}
