36229a437a6a5ce7f02a3cdc9eab545f46a4138b
oleg-ok12 <oleg.ok12@gmail.com>
2015-01-29 01:13:32 +0100

    troche CPCC

---
+++ Klient/ClientNode/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientNode
+{
+    public class Message
+    {
+        public string source_component_name;
+        public List<int> source_component_id;
+        public string dest_component_name;
+        public List<int> dest_component_id;
+        public List<String> parameters;//jakies parametry pakietu
+
+        public Message()
+        {
+            parameters = new List<String>();
+            source_component_id = new List<int>();
+            dest_component_id = new List<int>();
+        }
+
+        public Message(string scn, List<int> sc_id, string dcn, List<int> ds_id, List<String> param)
+        {
+            source_component_name = scn;
+            source_component_id = sc_id;
+            dest_component_name = dcn;
+            dest_component_id = ds_id;
+
+            parameters = param;
+
+        }
+
+
+    }
+}
