3dfbf663eb4a0da0053d69b150da4d251dd9dced
oleg-ok12 <oleg.ok12@gmail.com>
2015-01-30 00:02:27 +0100

    policydyrektory

--- NetworkCallController/NetworkCallController/PolicyDirectory.cs
+++ NetworkCallController/NetworkCallController/PolicyDirectory.cs
@@ -9,36 +9,35 @@ namespace NetworkCallController
     class PolicyDirectory
     {
         public SerializableDictionary<String, List<int>> slownik_adresow = new SerializableDictionary<string, List<int>>();
-        List<int> client1;
-        List<int> client2;
+        List<int> client1 = new List<int>();
+        List<int> client2 = new List<int>();
         List<int> pusta = new List<int>();
         public bool isAllow;
 
        public PolicyDirectory()
         {
 
-            isAllow = true;
-           List<int> client1 = new List<int>();
-           client1.Add(1);
-           client1.Add(0);
-           client1.Add(1);
-           List<int> client2 = new List<int>();
-           client1.Add(1);
-           client1.Add(0);
-           client1.Add(2);
-           slownik_adresow.Add("CLIENT1", client1);
-           slownik_adresow.Add("CLIENT2", client2);
-
+            isAllow = true;              
+                
+           
         }
 
        public List<int> askDirectory(String nazwa_klienta)
        {
            if (nazwa_klienta == "CLIENT1")
            {
+               client1.Add(1);
+               client1.Add(0);
+               client1.Add(1);
+               slownik_adresow.Add("CLIENT1", client1);
                return client1;
            }
            else if (nazwa_klienta == "CLIENT2")
            {
+               client1.Add(1);
+               client1.Add(0);
+               client1.Add(2);
+               slownik_adresow.Add("CLIENT2", client2);
                return client2;
            }
            return pusta;
